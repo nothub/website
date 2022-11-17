c5c1472a75edec0abee196815e2bad555d2c0a43
hub <code@hub.lol>
2022-11-17 15:48:07 +0100

    pin nix packages

--- shell.nix
+++ shell.nix
@@ -1,2 +1,7 @@
-{ pkgs ? import (fetchTarball "https://nixos.org/channels/nixos-22.05/nixexprs.tar.xz") { } }:
-with pkgs; mkShell { nativeBuildInputs = [ hugo html-tidy ]; }
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/814f8f3363cecee7cce314286846e4da5e92d689.tar.gz") { } }:
+with pkgs; mkShell {
+  nativeBuildInputs = [
+    hugo
+    html-tidy
+  ];
+}
