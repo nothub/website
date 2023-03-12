8ce1c159410f5eaba80e0c01e42605a9b7be46a7
Florian Hübner <code@hub.lol>
2023-03-12 12:06:25 +0100

    bump nix packages

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S nix-shell --pure
-let url = "https://github.com/NixOS/nixpkgs/archive/b69883faca9542d135fa6bab7928ff1b233c167f.tar.gz";
+let url = "https://github.com/NixOS/nixpkgs/archive/0c4800d579af4ed98ecc47d464a5e7b0870c4b1f.tar.gz";
 in { pkgs ? import (fetchTarball url) { } }:
 with pkgs; mkShell {
   nativeBuildInputs = [
