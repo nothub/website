1e19beaf6cf271a9912f5327821c24a7b72c58bc
Florian Hübner <code@hub.lol>
2023-04-17 17:19:34 +0200

    nix build tooling

--- shell.nix
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S nix-shell --pure
-let url = "https://github.com/NixOS/nixpkgs/archive/0c4800d579af4ed98ecc47d464a5e7b0870c4b1f.tar.gz";
-in { pkgs ? import (fetchTarball url) { } }:
-with pkgs; mkShell {
-  nativeBuildInputs = [
-    hugo
-    html-tidy
-  ];
-}
