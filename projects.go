96c5189a84668ba44505edca03e1d5b913e539cf
Florian Hübner <code@hub.lol>
2023-07-22 20:08:55 +0200

    projects

--- projects.go
+++ projects.go
@@ -7,8 +7,31 @@ import (
 	"net/http"
 )
 
-var projectsAuthor []string
-var projectsContrib []string
+// TODO: fetch infos from github api (https://github.com/orgs/community/discussions/24350)
+
+type project struct {
+	name  string
+	desc  string
+	url   string
+	langs []string
+	tags  []string
+	stars int
+}
+
+var langColors = make(map[string]string)
+
+func init() {
+	langColors["dockerfile"] = "#384D54"
+	langColors["go"] = "#00ADD8"
+	langColors["java"] = "#B07219"
+	langColors["lua"] = "#000080"
+	langColors["perl"] = "#0298C3"
+	langColors["python"] = "#3572A5"
+	langColors["shell"] = "#89E051"
+}
+
+var projectsAuthor []project
+var projectsContrib []project
 
 func initProjects(router *gin.Engine) (err error) {
 	log.Println("loading projects")
@@ -17,14 +40,14 @@ func initProjects(router *gin.Engine) (err error) {
 		return err
 	}
 
-	data := make(map[string][]string)
+	data := make(map[string][]project)
 	err = yaml.Unmarshal(file, &data)
 	if err != nil {
 		return err
 	}
 
-	projectsAuthor = data["authored"]
-	projectsContrib = data["contributed"]
+	projectsAuthor = data["author"]
+	projectsContrib = data["contrib"]
 
 	router.GET("/projects/*path", func(c *gin.Context) {
 		path := c.Param("path")
