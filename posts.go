6001c326ee46529433f3f889607dd66532ed8331
Florian Hübner <code@hub.lol>
2023-07-24 00:18:26 +0200

    goldmark links

--- posts.go
+++ posts.go
@@ -9,6 +9,12 @@ import (
 func initPosts(router *gin.Engine) (err error) {
 	// TODO
 
+	// https://github.com/yuin/goldmark
+	// https://github.com/yuin/goldmark-meta / https://github.com/abhinav/goldmark-frontmatter
+	// https://github.com/yuin/goldmark-highlighting
+	// https://github.com/abhinav/goldmark-anchor
+	// https://github.com/abhinav/goldmark-toc
+
 	router.GET("/posts/*path", func(c *gin.Context) {
 		path := c.Param("path")
 		log.Printf("path=%q\n", path)
