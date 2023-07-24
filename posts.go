8eb87d9680d0bd1af1ae35cfeac16519d12acda7
Florian Hübner <code@hub.lol>
2023-07-24 19:38:38 +0200

    pandoc render

--- posts.go
+++ posts.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"bytes"
 	"github.com/gin-gonic/gin"
-	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/renderer/html"
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func initPosts(router *gin.Engine) (err error) {
-	log.Println("rendering posts")
-
-	var gm = goldmark.New(goldmark.WithRendererOptions(html.WithUnsafe()))
+	log.Println("loading posts")
 
 	dir, err := fs.ReadDir("posts")
 	if err != nil {
@@ -26,54 +22,47 @@ func initPosts(router *gin.Engine) (err error) {
 		if entry.IsDir() {
 			continue
 		}
-		md, err := fs.ReadFile("posts/" + entry.Name())
+		slug := strings.TrimSuffix(entry.Name(), ".html")
+		log.Printf("loading post: %s\n", slug)
+
+		by, err := fs.ReadFile("posts/" + entry.Name())
 		if err != nil {
 			log.Fatalln(err.Error())
 		}
-		slug := entry.Name()
-		// TODO: sanitize slug
-		posts[slug] = renderPost(md, gm)
+
+		posts[slug] = template.HTML(by)
 	}
 
 	for _, entry := range dir {
 		if !entry.IsDir() {
 			continue
 		}
-		if slug, ok := posts[entry.Name()]; ok {
+		slug := strings.TrimSuffix(entry.Name(), ".html")
+		if _, ok := posts[slug]; ok {
 			log.Printf("found data for post: %s\n", slug)
 		}
 	}
 
 	router.GET("/posts", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "posts.tmpl", map[string]map[string]template.HTML{
-			"Posts": posts,
-		})
+		c.HTML(http.StatusOK, "posts.tmpl", posts)
 	})
 
-	router.GET("/posts/*path", func(c *gin.Context) {
-		path := c.Param("path")
-		if post, ok := posts[path]; ok {
+	router.GET("/posts/*slug", func(c *gin.Context) {
+		slug := strings.TrimPrefix(c.Param("slug"), "/")
+
+		// redirect to posts overview
+		if len(strings.TrimSpace(slug)) == 0 {
+			c.Request.URL.Path = "/posts"
+			router.HandleContext(c)
+			return
+		}
+
+		if post, ok := posts[slug]; ok {
 			c.HTML(http.StatusOK, "post.tmpl", post)
+		} else {
+			c.AbortWithStatus(http.StatusNotFound)
 		}
 	})
 
 	return nil
-}
-
-func renderPost(md []byte, gm goldmark.Markdown) template.HTML {
-	// TODO:
-	//   https://github.com/yuin/goldmark-meta / https://github.com/abhinav/goldmark-frontmatter
-	//   https://github.com/yuin/goldmark-highlighting
-	//   https://github.com/abhinav/goldmark-anchor
-	//   https://github.com/abhinav/goldmark-toc
-
-	var buf bytes.Buffer
-	err := gm.Convert(md, &buf)
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	log.Println(string(buf.Bytes()))
-
-	return template.HTML(buf.Bytes())
 }
