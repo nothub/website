8c0ec03e6d19390a4e52bfb39f38ad69a42d727d
Florian Hübner <code@hub.lol>
2023-07-31 16:28:24 +0200

    dayle star fetch

--- projects.go
+++ projects.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 type Project struct {
@@ -20,17 +21,19 @@ type Project struct {
 
 func initProjects(router *gin.Engine) (err error) {
 	log.Println("loading projects")
+
+	// load projects from data
 	bytes, err := fs.ReadFile("data/projects.yaml")
 	if err != nil {
 		return err
 	}
-
 	var projects []Project
 	err = yaml.Unmarshal(bytes, &projects)
 	if err != nil {
 		return err
 	}
 
+	// register tags
 	for _, project := range projects {
 		for _, tag := range project.Tags {
 			linkTag(tag, "Project: "+project.Title, project.Url)
@@ -40,10 +43,37 @@ func initProjects(router *gin.Engine) (err error) {
 		}
 	}
 
-	for i, proj := range projects {
+	// fetch stars on startup
+	go fetchStars(&projects)
+
+	// fetch stars again every day
+	ticker := time.NewTicker(24 * time.Hour)
+	go func() {
+		// when the server is shut down, this go routine will not be shutdown
+		// gracefully (it will just be killed), so do not do fancy stuff in here!
+		for {
+			select {
+			case <-ticker.C:
+				fetchStars(&projects)
+			}
+		}
+	}()
+
+	router.GET("/projects", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "projects.gohtml", projects)
+	})
+
+	return nil
+}
+
+func fetchStars(projects *[]Project) {
+	log.Printf("fetching github stars for %v projects\n", len(*projects))
+
+	for i, proj := range *projects {
+
 		u, err := url.Parse(proj.Url)
 		if err != nil {
-			log.Fatalln(err.Error())
+			log.Fatalf("invalid project url %s caused %s\n", proj.Url, err.Error())
 		}
 
 		if u.Host != "github.com" {
@@ -56,12 +86,6 @@ func initProjects(router *gin.Engine) (err error) {
 			continue
 		}
 
-		projects[i].Stars = meta.StargazersCount
+		(*projects)[i].Stars = meta.StargazersCount
 	}
-
-	router.GET("/projects", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "projects.gohtml", projects)
-	})
-
-	return nil
 }
