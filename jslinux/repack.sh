307f185a00665fd59c7fc9e143982b69d30f0efc
Florian Hübner <fhuebner@posteo.de>
2026-03-27 22:40:16 +0100

    jslinux fs repackaging

---
+++ jslinux/repack.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+set -eu
+cd "$(dirname "$(realpath "$0")")/root-x86"
+
+cleanup() {
+  sudo umount tmp/mount 2>/dev/null || true
+}
+trap cleanup EXIT
+
+mkdir -p tmp
+cat blk*.bin > tmp/rootfs.img
+
+mkdir -p tmp/mount
+sudo mount -o loop tmp/rootfs.img tmp/mount
+
+sudo cp -a includes/. tmp/mount
+
+sync
+sudo umount tmp/mount
+sudo rm -rf tmp/mount
+
+rm -f blk.txt blk*.bin
+split -b 256K -d -a 9 tmp/rootfs.img blk
+rm -rf tmp
+
+i=0
+for f in blk*; do
+  mv "${f}" "blk$(printf '%09d' ${i}).bin"
+  i=$((i+1))
+done
+
+for f in blk*.bin; do
+  truncate -s $((256*1024)) "${f}"
+done
+
+n=$(ls blk*.bin | wc -l)
+cat > blk.txt <<EOF
+{
+  block_size: 256,
+  n_block: $n,
+}
+EOF
--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,6 @@
 set -eu
 cd "$(dirname "$(realpath "$0")")/.."
 
+./jslinux/repack.sh
+
 GOOS=linux GOARCH=amd64 go build -tags osusergo,netgo,timetzdata -ldflags="-s -w" -o website
