b8372236cad530ac1f71597eed511cbd621d068a
Florian Hübner <fhuebner@posteo.de>
2026-06-15 23:14:51 +0200

    build: add OCI image labels

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,8 @@ cd "$(dirname "$(realpath "$0")")/.."
 TAG="${TAG:-dev}"
 CGO_ENABLED=0 GOOS=linux go build -trimpath -ldflags="-s -w" -o website .
 go tool ocipack \
-    -tag "ghcr.io/nothub/website:${TAG}" \
+    -tag "docker.io/n0thub/website:${TAG}" \
+    -label "org.opencontainers.image.source=https://github.com/nothub/website" \
+    -label "org.opencontainers.image.revision=$(git rev-parse HEAD)" \
     website image.tar.gz
 rm -f website
